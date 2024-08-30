0507cb73367b1d8d6dcecb95a90e7f8649c551af
SMei201900 <smei201900@gmail.com>
2024-08-29 21:56:40 -0400

    changed background color, changed gradient, added more text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-
-
 export default function Home() {
+	const textStyle = {
+		color: "white",
+	};
 
 	return (
-		<div>Hello!</div>
+		<div style={textStyle}>
+			<h1> Rate My Professor</h1>
+			<h2> Enter the name of the school</h2>
+			<h2> Enter the name of the professor</h2>
+		</div>
 	);
 }
