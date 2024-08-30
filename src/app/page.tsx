5f967610c36078b478b75270d77c05f6a1557f12
SMei201900 <smei201900@gmail.com>
2024-08-29 22:22:57 -0400

    made a button, made it round, going to do front-end edits to the textfields /typebox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
 
 	const overallStyle = {
 		display: "flex",
-		justifyContent: "center",
+		flexDirection: "column",
+		justifyContent: "space-between",
 		alignItems: "center",
 		height: "100vh",
 		textAlign: "center",
@@ -15,11 +16,26 @@ export default function Home() {
 		color: "white",
 	};
 
+	const buttonStyle = {
+		position: "absolute",
+		right: "20px",
+		top: "20px",
+		padding: "10px 20px",
+		fontSize: "20px",
+		backgroundColor: "#71AFFF",
+		color: "white",
+		border: "none",
+		borderRadius: "50px",
+	};
+
+	const inputStyle = {};
+
 	return (
 		<div style={overallStyle}>
 			<h1 style={headingOneSize}> Rate My Professor</h1>
-			<input type="text" placeholder="School Name" className="" />
-			<input type="text" placeholder="Professor Name" className="" />
+			<input type="text" placeholder="School Name" />
+			<input type="text" placeholder="Professor Name" />
+			<button style={buttonStyle}>Submit</button>
 		</div>
 	);
 }
